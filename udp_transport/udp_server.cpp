8db6a30b3de3358dd8f9d85fab95cd4ba417f092
quguiyou <quguiyou1076@qq.com>
2024-08-04 15:54:18 +0800

    服务端启动监听端口

--- udp_transport/udp_server.cpp
+++ udp_transport/udp_server.cpp
@@ -1,6 +1,36 @@
 #include "udp_server.h"
 
+#include <arpa/inet.h>
+
 namespace safe_udp {
+int UdpServer::StartServer(int port) {
+  int sfd;
+  struct sockaddr_in server_addr;
+  sfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+  if (sfd < 0) {
+    LOG(ERROR) << " Failed to socket !!!";
+    exit(0);
+  }
+
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  server_addr.sin_port = htons(port);
+
+  if (bind(sfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    LOG(ERROR) << "binding error !!!";
+    exit(0);
+  }
+
+  LOG(INFO) << "**Server Bind set to addr: " << server_addr.sin_addr.s_addr;
+  LOG(INFO) << "**Server Bind set to port: " << server_addr.sin_port;
+  LOG(INFO) << "**Server Bind set to family: " << server_addr.sin_family;
+  LOG(INFO) << "Started successfully";
+  sockfd_ = sfd;
+  return sfd;
+}
+
 bool UdpServer::OpenFile(const std::string &file_name) {
   LOG(INFO) << "Opening the file" << file_name;
 
